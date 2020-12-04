6272dae3305eeff191024eba50e3b7d9e180a7ec
Julien Bouland <bouland.julien@gmail.com>
2020-12-04 15:32:11 +0100

    Input component added

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ import {render} from "react-dom";
 import Link from "./Link.js";
 import Button from "./Button.js";
 import Container from "./Container.js";
+import Input from "./Input.js";
 
 function App() {
-    return (<>
-        <Link/>
-        <Button/>
-        <Container/>
-
-    </>);
+  return (<>
+      <Container>
+          <Link href="https://react-tutorial.app">React Tutorial</Link>
+          <Button disabled={true}>Login</Button>
+          <Input placeholder="Full Name" name="full_name" />
+      </Container>
+  </>);
 }
 
 render(<App />, document.querySelector("#react-root"));
