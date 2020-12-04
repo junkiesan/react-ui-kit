451817a6ba8d38fef3bb951b16c43cdda6fd48bd
Julien Bouland <bouland.julien@gmail.com>
2020-12-04 15:20:49 +0100

    Container component added

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ import React from "react";
 import {render} from "react-dom";
 import Link from "./Link.js";
 import Button from "./Button.js";
+import Container from "./Container.js";
 
 function App() {
     return (<>
         <Link/>
         <Button/>
+        <Container/>
+
     </>);
 }
 
